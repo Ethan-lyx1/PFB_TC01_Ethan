aaf1f9c3801b8baa481b59e878842e062ae5c5cc
HaziqBinHanafiah <157678113+HaziqBinHanafiah@users.noreply.github.com>
2024-01-26 19:35:27 +0800

    Update cash_on_hand.py
    Implemented Cash-on-Hand analysis functionality
    
    Added a functional Cash-on-Hand analysis function. The function is capable of computing the daily differences in Cash-on-Hand, determining whether it's consistently increasing, decreasing, or fluctuating, and returning relevant information such as the highest increment, highest decrement, or days with deficit amounts.
    
    The Cash-on-Hand analysis functionality is all encapsulated within a dedicated function, enhancing code modularization and facilitating ease of use from the main.py script.
    
    Signed-off-by: HaziqBinHanafiah <157678113+HaziqBinHanafiah@users.noreply.github.com>

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -2,19 +2,43 @@ from pathlib import Path
 import csv
 
 print (Path.cwd() )
+def cash_on_hand_analysis():
+    fp = Path.cwd() / "csv_reports" / "Cash_on_Hand.csv"
 
-fp = Path.cwd() / "csv_reports" / "Cash_on_Hand.csv"
+    with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+        file_reader = csv.reader(file)
+        next(file_reader) # skip header
 
-with fp.open(mode="r", encoding="UTF-8", newline="") as file:
-    file_reader = csv.reader(file)
-    next(file_reader) # skip header
+        Cash_on_Hand_Records=[] 
 
-    Cash_on_Hand_Records=[] 
+        for row in file_reader:
+            day = int(row[0])
+            cash_on_hand = float(row[1])  # Convert overhead value to a number
+            Cash_on_Hand_Records.append([day,cash_on_hand])
+           
+            daily_coh_difference = []
 
-    for row in file_reader:
-        day = float(row[0])
-        cash_on_hand = float(row[1])  # Convert overhead value to a number
-        Cash_on_Hand_Records.append([day,cash_on_hand])
+        for day_index in range(1, len(Cash_on_Hand_Records)):
+            previous_day_profit = Cash_on_Hand_Records[day_index - 1][1]
+            current_day_profit = Cash_on_Hand_Records[day_index][1]
+            difference = current_day_profit - previous_day_profit
+            daily_coh_difference.append((Cash_on_Hand_Records[day_index][0], difference))  # Tuple of day and difference
+        
+        is_increasing = all(diff[1] > 0 for diff in daily_coh_difference)
+        is_decreasing = all(diff[1] < 0 for diff in daily_coh_difference)
 
-print (Cash_on_Hand_Records)
+        if is_increasing:
+            highest_increment = max (daily_coh_difference)
+            return highest_increment
+        elif is_decreasing:
+            lowest_increment = min (daily_coh_difference)
+            return lowest_increment
+        else:
+            def custom_sort(item):
+                return item[1]
+            deficit_days = [(day, diff) for day, diff in daily_coh_difference if diff < 0]
+            deficit_days.sort(key=custom_sort)  # Sort by deficit amount
+            return deficit_days
+
+
 
