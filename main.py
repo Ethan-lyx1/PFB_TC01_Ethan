c6549044fc91eb9dc14949b72f94233d9cd53ea1
hilazine <158008413+hilazine@users.noreply.github.com>
2024-01-29 23:16:05 +0800

    Added comments to explain main.py
    Signed-off-by: hilazine <158008413+hilazine@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from pathlib import Path
+from pathlib import Path 
 import cash_on_hand
 import overheads
-import Profits_and_Loss
+import Profits_and_Loss #import other modules created
 
-fp = Path.cwd() / "summary_report.txt"
-with fp.open(mode="w", encoding="UTF-8") as file:
+fp = Path.cwd() / "summary_report.txt" #create file path for summary_report.txt to be written in
+
+with fp.open(mode="w", encoding="UTF-8") as file: # Open the file in write mode with UTF-8 encoding
+    # Write the analysis results from each module to the file
+    # Each result is written on a new line using \n
     file.write(overheads.read_over_and_find_highest_overhead() + "\n") 
     file.write(cash_on_hand.cash_on_hand_analysis() + "\n")
     file.write((Profits_and_Loss.PnL_analysis()))
